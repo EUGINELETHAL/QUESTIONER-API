60b0983f8f310dcefd14c138e050151ad138d9b7
EUGINELETHAL <ochungeugine@gmail.com>
2019-01-10 10:46:13 +0300

    [finishes  #163103993]setting up directory structure

--- run.py
+++ run.py
@@ -7,5 +7,6 @@ config_name = os.getenv("APP_SETTINGS")
 """Gets the app settings defined in the .env file"""
 app = create_app(config_name)
 """defining the configuration to be used"""
+
 if __name__ == '__main__':
-app.run()
+    app.run()
