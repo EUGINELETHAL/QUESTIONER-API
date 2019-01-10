7f2348eb7edbbca9a0c84ef940bbf0eaacc2e2dc
EUGINELETHAL <ochungeugine@gmail.com>
2019-01-10 17:10:26 +0300

    [starts #163111672] created and initialized meetup class

---
+++ app/API/models.py
@@ -0,0 +1,11 @@
+class Meetup:
+    """
+    This class represents the meetup model.
+    """
+    def __init__(self, meetup_list):
+        """
+        Initialize meetups list.
+        """
+        self.meetup_list = meetup_list
+
+    
