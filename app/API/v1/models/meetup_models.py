80a5bfeec45b711617cac0396725c9ddde4babc5
EUGINELETHAL <ochungeugine@gmail.com>
2019-01-11 11:51:53 +0300

    [starts #163142146] added views for getimng meetups

--- app/API/v1/models/meetup_models.py
+++ app/API/v1/models/meetup_models.py
@@ -1,39 +1,5 @@
-meetup_list = []
-
-"""
-    This class represents the meetup model.
-
-    """
-class Meetup:
-    def __init__(self, meetup_list):
-        """
-        Initialize meetups list.
-        """
-        self.meetup_list = meetup_list
-
-    def show_meetups(self):
-        """
-        Show the meetups.
-        """
-        return self.meetup_list
-
-    def add_meetup(self,meetup):
-        """
-        Append meetups to the meetup_list.
-        """
-        meetup = {
+meetup = {
             "meetup_id" : 1,
         "Createdon":"Monday",
         "Location":"Andela",
         "HappeningOn":"Tuesday"}
-
-        self.meetup_list.append(meetup)
-
-    def delete_meetup(self, index):
-        """
-        Delete meetup.
-        """
-        del_meetup = [del_meetup for del_meetup in self.meetup_list if del_meetup["id"] == index]
-        if del_meetup:
-            self.meetup_list.remove(del_meetup[0])
-            return True 
--- app/API/v1/views.py
+++ app/API/v1/views.py
@@ -1,37 +1,13 @@
 from flask import Flask
 from flask import request, jsonify, abort
 from .import v1
-from app.API.v1.models.meetup_models import Meetup
+from app.API.v1.models. meetup_models import meetup
 
-@v1.route('/api/vi/post_meetup/', methods=["POST"])
-def post_meetup(self):
-        """
-    Method to Posts a new meetup
-        responses:
-        200:
-            description: The request was successful.
-        201:
-            description: New request created.
-        400:
-            description: Bad request made.
-        401:
-            description: Unauthorised access.
-        404:
-            description: Page not found.
-        """
-        if 'topic' in request.json and not request.json['topic']:
-            return {"message": "topic is required"}, 400
 
-        createdOn, location,topic,happeningOn = request.json['createdOn'], request.json['location'], request.json['topic'], request.json['happeningOn']
-        post_meetup = {
-       'meetup_id': meetup[-1]['meetup_id'] + 1,
-       'createdOn': createdOn,
-       "location" : location,
-       "topic" : topic,
-       "happeningOn" :happeningOn
-    }
-        meetup.add_meetup(post_meetup)
 
-        return jsonify({'status': 201,
-                       "message": "request successfull!",
-                       "post_meetup":post_meetup}),201
+@v1.route('/api/vi/meetups/', methods=["GET"])
+def get_all_meetups():
+    """
+    View all meetups.
+    """
+    return jsonify({'meetup':meetup}), 200
