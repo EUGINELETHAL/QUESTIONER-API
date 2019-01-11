f039e7cd9cdc6e73615a949679818b140f999a82
EUGINELETHAL <ochungeugine@gmail.com>
2019-01-11 16:17:31 +0300

    [starts #163144613]added routes for getting a specific meetup '

--- app/API/v1/models/meetup_models.py
+++ app/API/v1/models/meetup_models.py
@@ -1,6 +1,15 @@
-meetup = {
-        "meetup_id" : 1,
+meetups= [{
+        "id" : 1,
         "Createdon":"Monday",
         "Location":"Andela",
-        "HappeningOn":"20.7.2018",
-}
+        "HappeningOn":"20.7.2018"},
+
+
+      {  "id" : 2,
+        "Createdon":"Tuesday",
+        "Location":"Andela",
+        "HappeningOn":"25.7.2018"}
+
+
+
+]
