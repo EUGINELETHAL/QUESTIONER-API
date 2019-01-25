41fb318ffedcae4d6cd30e655083b2449828e1b3
EUGINELETHAL <ochungeugine@gmail.com>
2019-01-25 22:45:23 +0300

    [finishes #163163817] fixes getting meetup endpoint

--- app/API/v1/views.py
+++ app/API/v1/views.py
@@ -8,6 +8,9 @@ def post_meetup():
     '''post meetup'''
 
     meetupdata = request.get_json()
+    if not meetupdata:
+        return jsonify({"status": 400, 
+        "message": "expects only Application/JSON data"}), 400
     
     topic = meetupdata('topic')
     location = meetupdata('location')
@@ -18,10 +21,22 @@ def post_meetup():
     new_meetup = Meetup(topic, createdon, location, happeningon, tags)
     added_meetup = new_meetup.save_meetup()
 
-    return jsonify({'meetup': added_meetup}), 201
+    return jsonify({'meetup': added_meetup, "status": 201, "message":
+                    "meetup created  sucessfully"})
 
 
 @v1.route('/api/v1/meetups/', methods=["GET"])
 def get_meetups():
     
     return jsonify(Meetup.meetup_list)
+
+
+@v1.route('/api/v1/meetups/<meetupId>', methods=["GET"])
+def fetch_single_meetup(meetupId):
+        """Deals with fetching a single question."""
+        meetups = Meetup.meetup_list
+        single_meetup = [mp for mp in meetups if mp["meeetupId"] == meetupId]
+        if single_meetup:
+            return single_meetup
+        else:
+            return "meetup Not Found"
